9600248f794687f459d0ac46171ae668bb505d9d
Jens Torgeir Solaugsten <jens-torgeir.solaugsten@sarpsborg.com>
2020-10-28 11:06:37 +0100

    Add IoTMessage

---
+++ elsys_decoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sarpsborgkommune.iot
+{
+    class ElsysDecoder : MessageDecoder
+    {
+        public override string Decode()
+        {
+            return "";
+        }
+    }
+}
